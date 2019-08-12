ee39d1a185dff10b12a812021dfd7af29af51a5a
jbautista <bautistajaycell@gmail.com>
2019-08-12 23:47:22 +0800

    Added the main tab bar controller and make it as the root view controller

--- Podcast/Podcast/MainTabBarController.swift
+++ Podcast/Podcast/MainTabBarController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainTabBarController.swift
 //  Podcast
 //
 //  Created by Jaycell on 12/08/2019.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any aditional setup after loading the view.
+        
+        view.backgroundColor = .green
     }
-
-
 }
-
