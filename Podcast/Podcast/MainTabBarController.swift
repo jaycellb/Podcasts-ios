986caf428d509cb30dc5c2b695d3048e1d9022c6
jbautista <bautistajaycell@gmail.com>
2019-08-15 01:06:01 +0800

    Added Title on the Menu Tab.

--- Podcast/Podcast/MainTabBarController.swift
+++ Podcast/Podcast/MainTabBarController.swift
@@ -23,6 +23,7 @@ class MainTabBarController: UITabBarController {
     
     fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
         let navController = UINavigationController(rootViewController: rootViewController)
+        rootViewController.navigationItem.title = title
         navController.tabBarItem.title = title
         navController.tabBarItem.image = image
         return navController
