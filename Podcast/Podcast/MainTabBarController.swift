5a8e9e46e5e83cb3ac8186f002b3314aa02b9fe5
jbautista <bautistajaycell@gmail.com>
2019-08-15 01:26:54 +0800

    Included large titles on the Menu Tab and setup setupViewControllers function.

--- Podcast/Podcast/MainTabBarController.swift
+++ Podcast/Podcast/MainTabBarController.swift
@@ -12,14 +12,25 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
         tabBar.tintColor = .purple
-
+        
+        setupViewControllers()
+    }
+    
+    //Jay:- Setup Functions
+    
+    func setupViewControllers() {
         viewControllers = [
             generateNavigationController(for: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites")),
             generateNavigationController(for: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search")),
             generateNavigationController(for: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
         ]
-        }
+    }
+    
+    
+    //Jay:- Helper Functions
     
     fileprivate func generateNavigationController(for rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
         let navController = UINavigationController(rootViewController: rootViewController)
