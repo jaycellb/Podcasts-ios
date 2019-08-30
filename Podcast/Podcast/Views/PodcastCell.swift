5ae3e1b0cfe4be02baeccdf1315b55aad323e322
jbautista <bautistajaycell@gmail.com>
2019-08-30 23:58:05 +0800

    Code to display the downloaded image from mzstatic.com to podcastImageView inside the PodcastCell.xib.

--- Podcast/Podcast/Views/PodcastCell.swift
+++ Podcast/Podcast/Views/PodcastCell.swift
@@ -22,9 +22,20 @@ class PodcastCell: UITableViewCell {
         artistNameLabel.text = podcast.artistName
             
         episodeCountLabel.text = "\(podcast.trackCount ?? 0) Episodes"
-        }
+            
+        print("Loading image with URL:", podcast.artworkUrl600 ?? "")
+            
+        guard let url = URL(string: podcast.artworkUrl600 ?? "") else {
+                return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            print("Finished downloading data:", data)
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.podcastImageView.image = UIImage(data: data)
+            }
+        }.resume()
     
     }
     
 }
-
+}
