722326e9584bed988cf29b20b369c6b1441aedc2
jbautista <bautistajaycell@gmail.com>
2019-09-01 01:13:38 +0800

    Installed SDWebImage to support caching. Used SDWebImage to cache icons.

--- Podcast/Podcast/Views/PodcastCell.swift
+++ Podcast/Podcast/Views/PodcastCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class PodcastCell: UITableViewCell {
     
@@ -27,14 +28,16 @@ class PodcastCell: UITableViewCell {
             
         guard let url = URL(string: podcast.artworkUrl600 ?? "") else {
                 return }
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
-            print("Finished downloading data:", data)
-            guard let data = data else { return }
-            DispatchQueue.main.async {
-                self.podcastImageView.image = UIImage(data: data)
-            }
-        }.resume()
+//        URLSession.shared.dataTask(with: url) { (data, _, _) in
+//            print("Finished downloading data:", data)
+//            guard let data = data else { return }
+//            DispatchQueue.main.async {
+//                self.podcastImageView.image = UIImage(data: data)
+//            }
+//        }.resume()
     
+        podcastImageView.sd_setImage(with: url, completed: nil)
+            
     }
     
 }
