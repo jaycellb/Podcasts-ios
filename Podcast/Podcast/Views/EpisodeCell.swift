f5058dafa156485ae4e981acd314e676725270ce
jbautista <bautistajaycell@gmail.com>
2019-09-08 01:48:36 +0800

    Set number of lines for titleLabel and descriptionLavel to 2. Set the spacing of titleLabel, pubDate, and descriptionLabel to 4.

--- Podcast/Podcast/Views/EpisodeCell.swift
+++ Podcast/Podcast/Views/EpisodeCell.swift
@@ -12,6 +12,17 @@ class EpisodeCell: UITableViewCell {
 
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBOutlet weak var pubDateLabel: UILabel!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 2
+        }
+        
+    }
+    
+    @IBOutlet weak var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.numberOfLines = 2
+        }
+        
+    }
 }
