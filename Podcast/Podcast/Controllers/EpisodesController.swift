2abbc534af7a96e25dce5aa4d72b614448c64d1f
jbautista <bautistajaycell@gmail.com>
2019-09-09 23:14:47 +0800

    Set the imageUrl of the episode to be taken from the podcast.

--- Podcast/Podcast/Controllers/EpisodesController.swift
+++ Podcast/Podcast/Controllers/EpisodesController.swift
@@ -33,9 +33,13 @@ class EpisodesController: UITableViewController {
             
             switch result {
             case let .rss(feed):
+                
+                let imageUrl = feed.iTunes?.iTunesImage?.attributes?.href
+                
                 var episodes = [Episode]() //blank Episode array
                 feed.items?.forEach({ (feedItem) in
-                   let episode = Episode(feedItem: feedItem)
+                    var episode = Episode(feedItem: feedItem)
+                    episode.imageUrl = imageUrl
                     episodes.append(episode)
                 })
                 self.episodes = episodes
