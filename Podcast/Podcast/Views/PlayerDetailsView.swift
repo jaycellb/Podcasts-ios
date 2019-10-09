19a863ca8ff5cdbec2d525d68e5c9631d706d7b2
jbautista <bautistajaycell@gmail.com>
2019-10-10 00:08:16 +0800

    Created the function player to play the audio from the url obtained in the playerItem.

--- Podcast/Podcast/Views/PlayerDetailsView.swift
+++ Podcast/Podcast/Views/PlayerDetailsView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 class PlayerDetailsView: UIView {
     
@@ -24,7 +25,17 @@ class PlayerDetailsView: UIView {
    
     fileprivate func playEpisode() {
         print("Trying to play episode at url:", episode.streamUrl)
+        
+        guard let url = URL(string: episode.streamUrl) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
     }
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        return avPlayer
+    }()
     
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
