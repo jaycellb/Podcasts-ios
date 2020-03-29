00a6a86995f0a8ba4da487a5b5c0e1b1ec2d2786
Jaycell <>
2020-03-29 19:48:18 +0800

    Refactor code for UIPanGestureRecognizer for .changed and .ended state.

--- Podcast/Podcast/Views/PlayerDetailsView.swift
+++ Podcast/Podcast/Views/PlayerDetailsView.swift
@@ -78,35 +78,34 @@ class PlayerDetailsView: UIView {
     
     @objc func handlePan(gesture: UIPanGestureRecognizer) {
 //        print("Panning")
-        if gesture.state == .began {
-            print("Began")
-        } else if gesture.state == .changed {
-            print ("Changed")
-            let translation = gesture.translation(in: self.superview)
-            self.transform = CGAffineTransform(translationX: 0, y: translation.y)
-            
-            self.maximizedStackView.alpha = -translation.y / 200
-            self.miniPlayerView.alpha = 1 + translation.y / 200
-            
+        if gesture.state == .changed {
+            handlePanChaged(gesture: gesture)
         } else if gesture.state == .ended {
-            let translation = gesture.translation(in: self.superview)
-            let velocity = gesture.velocity(in: self.superview)
-            
-            print("Ended", velocity.y)
-            
-            UIView.animate(withDuration: 0.5 , delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-                
-                self.transform = .identity
-                
-                if translation.y < -200 || velocity.y < -500 {
-                    let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
-                    mainTabBarController?.maximizePlayerDetails(episode: nil)
-                } else {
-                    self.miniPlayerView.alpha = 1
-                    self.maximizedStackView.alpha = 0
-                }
-            })
+            handlePanEnded(gesture: gesture)
         }
+    }
+    
+    func handlePanChaged(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        self.transform = CGAffineTransform(translationX: 0, y: translation.y)
+        self.maximizedStackView.alpha = -translation.y / 200
+        self.miniPlayerView.alpha = 1 + translation.y / 200
+    }
+    
+    func handlePanEnded(gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.superview)
+        let velocity = gesture.velocity(in: self.superview)
+        print("Ended", velocity.y)
+        UIView.animate(withDuration: 0.5 , delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.transform = .identity
+            if translation.y < -200 || velocity.y < -500 {
+                let mainTabBarController = UIApplication.shared.keyWindow?.rootViewController as? MainTabBarController
+                mainTabBarController?.maximizePlayerDetails(episode: nil)
+            } else {
+                self.miniPlayerView.alpha = 1
+                self.maximizedStackView.alpha = 0
+            }
+        })
     }
     
     @objc func handleTapMaximize() {
