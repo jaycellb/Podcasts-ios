8edf6ecb2c31330e964e36248087e43adb2f3f4f
Jaycell <>
2020-03-30 16:15:42 +0800

    Manipulated the anchors so that when the miniPlayerView was dragged upward, the episode list would not show in the background.

--- Podcast/Podcast/MainTabBarController.swift
+++ Podcast/Podcast/MainTabBarController.swift
@@ -24,8 +24,8 @@ class MainTabBarController: UITabBarController {
     
     @objc func minimizePlayerDetails() {
         maximizedTopAnchorConstraint.isActive = false
-        
         minimizedTopAnchorConstraint.isActive = true
+        bottomAnchorConstraint.constant = view.frame.height
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             
@@ -43,6 +43,8 @@ class MainTabBarController: UITabBarController {
         maximizedTopAnchorConstraint.isActive = true
         maximizedTopAnchorConstraint.constant = 0
         minimizedTopAnchorConstraint.isActive = false
+        
+        bottomAnchorConstraint.constant = 0
         
         if episode != nil{
         playerDetailsView.episode = episode
@@ -65,6 +67,8 @@ class MainTabBarController: UITabBarController {
     
     var maximizedTopAnchorConstraint: NSLayoutConstraint!
     var minimizedTopAnchorConstraint: NSLayoutConstraint!
+    var bottomAnchorConstraint: NSLayoutConstraint!
+    
     
     //Calls PodcastsSearchController to the MainTabBar
     fileprivate func setUpPlayerDetailsView() {
@@ -79,16 +83,16 @@ class MainTabBarController: UITabBarController {
         playerDetailsView.translatesAutoresizingMaskIntoConstraints = false
         
         //set anchors
-        
         maximizedTopAnchorConstraint = playerDetailsView.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height)
-
         maximizedTopAnchorConstraint.isActive = true
+        
+        bottomAnchorConstraint = playerDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: view.frame.height)
+        bottomAnchorConstraint.isActive = true
         
         minimizedTopAnchorConstraint = playerDetailsView.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -64)
 //        minimizedTopAnchorConstraint.isActive = true
         
         playerDetailsView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        playerDetailsView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         playerDetailsView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
     }
