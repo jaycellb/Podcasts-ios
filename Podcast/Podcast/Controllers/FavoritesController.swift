c278420e79900219d299fa27d4f5c1522b1fa3df
Jaycell <>
2020-04-29 23:15:27 +0800

    Just a few refactoring.

--- Podcast/Podcast/Controllers/FavoritesController.swift
+++ Podcast/Podcast/Controllers/FavoritesController.swift
@@ -13,18 +13,23 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     fileprivate let cellId = "cellId"
     
     override func viewDidLoad() {
-
-        
         super.viewDidLoad()
         
+        setupCollectionView()
+    
+    }
+    
+    fileprivate func setupCollectionView() {
         collectionView?.backgroundColor = .blue
         collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
+    //MARK:- UICollectionView Delegate / Spacing Methods
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
-    
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
         cell.backgroundColor = .red
