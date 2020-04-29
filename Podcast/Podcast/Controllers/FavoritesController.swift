1b4ca67d3f9038c1c96b65e4d1a2712b1374db7a
Jaycell <>
2020-04-29 23:08:25 +0800

    Set an edge inset of 16 on top, bottom, left and right of the cells. Set the width and height of the cells by getting the value of length of the frame and subtract it by 3 multiplied by 16 for the borders and finally divide the  value by 2.

--- Podcast/Podcast/Controllers/FavoritesController.swift
+++ Podcast/Podcast/Controllers/FavoritesController.swift
@@ -32,9 +32,14 @@ class FavoritesController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: 100)
+        
+        let width = (view.frame.width - 3 * 16) / 2
+        
+        return CGSize(width: width, height: width)
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+    }
     
 }
