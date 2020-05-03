7c53045151f3352bc5e3b3b10b723e97f5bd1b8d
Jaycell <>
2020-05-03 10:40:22 +0800

    Fetch the array from the UserDefaults first before saving data to it.

--- Podcast/Podcast/Controllers/EpisodesController.swift
+++ Podcast/Podcast/Controllers/EpisodesController.swift
@@ -70,8 +70,13 @@ class EpisodesController: UITableViewController {
         
         guard let podcast = self.podcast else { return }
         
+        // Fetched saved podcast
+        guard let savedPodcastsData = UserDefaults.standard.data(forKey: favoritedPodcastKey) else { return }
+        guard let savedPodcasts = NSKeyedUnarchiver.unarchiveObject(with: savedPodcastsData) as? [Podcast] else { return }
+        
+        
         //1. Transform Podcast into data
-        var listOfPodcasts = [Podcast]()
+        var listOfPodcasts = savedPodcasts 
         listOfPodcasts.append(podcast)
         let data = NSKeyedArchiver.archivedData(withRootObject: listOfPodcasts)
         UserDefaults.standard.set(data, forKey: favoritedPodcastKey)
---
+++ Podcast/Podcast/Extensions/UserDefaults.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaults.swift
+//   
+//
+//  Created by Jaycell on 5/3/20.
+//  Copyright © 2020 Jaycell. All rights reserved.
+//
+
+import Foundation
