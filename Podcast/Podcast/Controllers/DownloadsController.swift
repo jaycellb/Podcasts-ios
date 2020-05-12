ed0b7c95219b22b6ad72f8a6bcd20c60b769f7ae
Jaycell <>
2020-05-12 10:09:24 +0800

    Use existing nib EpisodeCell as structure for the EpisodeCell in the Downloads Tab Bar.

--- Podcast/Podcast/Controllers/DownloadsController.swift
+++ Podcast/Podcast/Controllers/DownloadsController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class DownloadsController: UITableViewController {
     
+    fileprivate let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.backgroundColor = .red
+    setupTableView()
         
     }
     
+//    MARK:- Setup
+    
+    fileprivate func setupTableView() {
+        
+        let nib = UINib(nibName: "EpisodeCell", bundle: nil)
+        tableView.register(nib , forCellReuseIdentifier: cellId )
+        
+    }
+    
+//    MARK:- UITABLEVIEW
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! EpisodeCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 134
+    }
 }
