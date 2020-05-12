e5e7a87ffa08c27f6f4afb9d5e6cad2dd74a7f09
Jaycell <>
2020-05-12 09:46:20 +0800

    Created DownloadsController file to where codes for the downloads will be written. Set the background color to red to check if it's working.

---
+++ Podcast/Podcast/Controllers/DownloadsController.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadController.swift
+//   
+//
+//  Created by Jaycell on 5/11/20.
+//  Copyright © 2020 Jaycell. All rights reserved.
+//
+
+import UIKit
+
+class DownloadsController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .red
+        
+    }
+    
+}
